cc02f7189a18d7823761c364be0c9573b0900610
TheGoldKay <jhonnatan.flr@gmail.com>
2024-02-14 17:10:46 -0300

    full encryption

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,17 @@
 import os
 from cryptography.fernet import Fernet
+from pyminizip import compress
+
+def protect_key(key_name):
+    pw = input("Enter password for key.key: ")
+    name = key_name.replace(".key", "")
+    zip_file = f"{name}.zip"
+
+    # Compress the file with password protection
+    compress(key_name, None, zip_file, pw, 1)
+
+    print(f"File compressed and password protected as {zip_file}")
+
 
 def save_key(filename):
     """
@@ -11,10 +23,38 @@ def save_key(filename):
             f.write(key)
             print(f"Saved content to {filename}")
             print(f"KEY: {key}")
+            print("------------------------------ Now set a password for the key --------------------------")
+            protect_key(filename)
     else:
         print(f"File -> {filename} <- already exists, skipping save.")
 
+def crypt(bitwarden_csv, key_name):
+    # opening the key
+    with open(key_name, 'rb') as filekey:
+        key = filekey.read()
 
+    # using the generated key
+    fernet = Fernet(key)
+
+    # opening the original file to encrypt
+    with open(bitwarden_csv, 'rb') as file:
+        original = file.read()
+
+    # encrypting the file
+    encrypted = fernet.encrypt(original)
+
+    # opening the file in write mode and 
+    # writing the encrypted data
+    with open(bitwarden_csv, 'wb') as encrypted_file:
+        encrypted_file.write(encrypted)
+    print(f"File -> {bitwarden_csv} <- encrypted.")
+
+def delete_key(key_name):
+    os.remove(key_name)
 
 if __name__ == "__main__":
-    save_key("fileKey.key")
+    bitwarden_csv = "bitwarden.csv"
+    key_name = "bitwarden.key"
+    save_key(key_name)
+    crypt(bitwarden_csv, key_name)
+    delete_key(key_name)
